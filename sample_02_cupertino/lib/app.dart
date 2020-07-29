a45a2d1430bb9a3d862b0b0a0dd505f03c285cfc
壹刀流 <idevebi@163.com>
2020-07-29 16:38:42 +0800

    sample 02: create cupertino app  and home page.

---
+++ sample_02_cupertino/lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// 仓库 App 类
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: new CupertinoStoreHomePage(),
+    );
+  }
+}
+
+/// 仓库首页
+class CupertinoStoreHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(middle: const Text('商品列表')),
+      child: Container(color: Colors.blue),
+    );
+  }
+}
