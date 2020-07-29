7989e8854631407420ac962080693ebf110b7876
壹刀流 <idevebi@163.com>
2020-07-30 05:32:33 +0800

    sample 02: create tab scaffold

--- sample_02_cupertino/lib/app.dart
+++ sample_02_cupertino/lib/app.dart
@@ -16,10 +16,46 @@ class CupertinoStoreApp extends StatelessWidget {
 /// 仓库首页
 class CupertinoStoreHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(middle: const Text('商品列表')),
-      child: Container(color: Colors.blue),
-    );
+  Widget build(Object context) => CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              title: Text('商品'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.search),
+              title: Text('搜索'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.shopping_cart),
+              title: Text('购物车'),
+            ),
+          ],
+        ),
+        tabBuilder: (context, index) => CupertinoTabView(
+          builder: (context) => CupertinoPageScaffold(
+            child: _pageScaffoldChild(index),
+          ),
+        ),
+      );
+
+  Widget _pageScaffoldChild(int index) {
+    switch (index) {
+      case 0:
+        return Container(
+          color: Colors.red,
+        );
+      case 1:
+        return Container(
+          color: Colors.blue,
+        );
+      case 2:
+        return Container(
+          color: Colors.green,
+        );
+      default:
+        throw ('$index is out of range.');
+    }
   }
 }
