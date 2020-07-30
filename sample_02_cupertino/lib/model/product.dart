b122cad3366533afc083cb0128231742cae1909c
壹刀流 <idevebi@163.com>
2020-07-30 16:06:06 +0800

    fix: asset paackage error.

--- sample_02_cupertino/lib/model/product.dart
+++ sample_02_cupertino/lib/model/product.dart
@@ -29,7 +29,7 @@ class Product {
   final int price;
 
   String get assetName => '$id-0.jpg';
-  String get assetPackage => 'shhrine_images';
+  String get assetPackage => 'shrine_images';
 
   @override
   String toString() => '$name (id=$id)';
