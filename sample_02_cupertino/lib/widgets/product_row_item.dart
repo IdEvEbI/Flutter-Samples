8bbba4c2a7ebaf5cf4ce0dd415749410e6861fff
壹刀流 <idevebi@163.com>
2020-07-30 18:22:56 +0800

    sample 02: product row item layout

--- sample_02_cupertino/lib/widgets/product_row_item.dart
+++ sample_02_cupertino/lib/widgets/product_row_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../model/product.dart';
+import '../style.dart';
 
 /// 商品行元素
 class ProductRowItem extends StatelessWidget {
@@ -13,6 +14,65 @@ class ProductRowItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(product.name);
+    final row = SafeArea(
+      top: false,
+      bottom: false,
+      minimum: const EdgeInsets.only(left: 16, top: 8, bottom: 8, right: 8),
+      child: Row(
+        children: <Widget>[
+          // 商品图片
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Image.asset(
+              product.assetName,
+              package: product.assetPackage,
+              fit: BoxFit.cover,
+              width: 76,
+              height: 76,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    product.name,
+                    style: Styles.productRowItemName,
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 8)),
+                  Text(
+                    '\$${product.price}',
+                    style: Styles.productRowItemPrice,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (lastItem) {
+      return row;
+    }
+
+    return Column(
+      children: <Widget>[
+        row,
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 100,
+            right: 16,
+          ),
+          child: Divider(
+            height: 1,
+            color: Styles.productRowDivider,
+          ),
+        ),
+      ],
+    );
   }
 }
