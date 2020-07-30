6e9d9e22e0a8341901c8be198f44aad1128c6363
壹刀流 <idevebi@163.com>
2020-07-31 07:01:20 +0800

    sample 02: init and dispose private state properties.

--- sample_02_cupertino/lib/widgets/search_tab.dart
+++ sample_02_cupertino/lib/widgets/search_tab.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/cupertino.dart';
 
+/// 搜索标签页
 class SearchTab extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SearchTabState();
 }
 
+/// 搜索标签状态
 class _SearchTabState extends State<SearchTab> {
+  TextEditingController _controller;
+  FocusNode _focusNode;
+  String _terms = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = TextEditingController()
+      ..addListener(() {
+        setState(() {
+          _terms = _controller.text;
+        });
+      });
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _controller.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
