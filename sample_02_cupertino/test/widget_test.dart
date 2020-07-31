c5f0b12b111a958cf091f33d171e289ea078b5d0
壹刀流 <idevebi@163.com>
2020-08-01 05:44:25 +0800

    fix: widget_test

--- sample_02_cupertino/test/widget_test.dart
+++ sample_02_cupertino/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sample_02_cupertino/main.dart';
+import 'package:sample_02_cupertino/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(CupertinoStoreApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
