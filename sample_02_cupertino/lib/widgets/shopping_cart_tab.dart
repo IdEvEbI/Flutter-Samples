a0f52f7b25d6e548560020be0765d2817e09d4d1
壹刀流 <idevebi@163.com>
2020-08-01 05:43:06 +0800

    sample 02: customer info layout.

--- sample_02_cupertino/lib/widgets/shopping_cart_tab.dart
+++ sample_02_cupertino/lib/widgets/shopping_cart_tab.dart
@@ -9,6 +9,12 @@ class ShoppingCartTab extends StatefulWidget {
 }
 
 class _ShoppingCartTabState extends State<ShoppingCartTab> {
+  String name;
+  String email;
+  String location;
+  String pin;
+  DateTime dateTime = DateTime.now();
+
   @override
   Widget build(BuildContext context) => Consumer<AppStateModel>(
         builder: (context, model, child) => CustomScrollView(
@@ -16,7 +22,64 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
             CupertinoSliverNavigationBar(
               largeTitle: Text('购物车'),
             ),
+            SliverSafeArea(
+              top: false,
+              minimum: const EdgeInsets.only(top: 4),
+              sliver: SliverList(
+                delegate: _buildSliverChildBuilderDelegate(model),
+              ),
+            ),
           ],
         ),
       );
+
+  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
+      AppStateModel model) {
+    return SliverChildBuilderDelegate((context, index) {
+      if (index > 2) {
+        return null;
+      }
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: _buildTextField(index),
+      );
+    });
+  }
+
+  CupertinoTextField _buildTextField(int index) {
+    if (index > 2) {
+      return null;
+    }
+
+    String placeholder = '请输入';
+    IconData icon;
+
+    if (index == 0) {
+      placeholder += '姓名';
+      icon = CupertinoIcons.person_solid;
+    } else if (index == 1) {
+      placeholder += '邮箱';
+      icon = CupertinoIcons.mail_solid;
+    } else {
+      placeholder += '地址';
+      icon = CupertinoIcons.location_solid;
+    }
+
+    return CupertinoTextField(
+      prefix: Icon(
+        icon,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      autocorrect: false,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 0, color: CupertinoColors.inactiveGray),
+        ),
+      ),
+      placeholder: placeholder,
+    );
+  }
 }
