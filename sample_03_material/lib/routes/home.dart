6b398543f3ebf7c17f15273a5244dc774093d725
壹刀流 <idevebi@163.com>
2020-08-04 04:19:56 +0800

    sample 03: app bar, leading icon and actions icons.

--- sample_03_material/lib/routes/home.dart
+++ sample_03_material/lib/routes/home.dart
@@ -3,6 +3,38 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('界面布局示例'),
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              semanticLabel: '菜单',
+            ),
+            onPressed: () {
+              print('点击菜单按钮');
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                semanticLabel: '搜索',
+              ),
+              onPressed: () {
+                print('点击搜索按钮');
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.tune,
+                semanticLabel: '过滤',
+              ),
+              onPressed: () {
+                print('点击过滤按钮');
+              },
+            ),
+          ],
+        ),
         body: Center(
           child: Text('Hello material'),
         ),
