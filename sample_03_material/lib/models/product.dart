0480eda9993ecb87a72196aff4aa48ed90af615d
壹刀流 <idevebi@163.com>
2020-08-04 05:46:48 +0800

    sample 03: show product list in grid view.

---
+++ sample_03_material/lib/models/product.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+
+/// 商品分类
+enum Category {
+  all,
+  accessories,
+  clothing,
+  home,
+}
+
+/// 商品
+class Product {
+  const Product({
+    @required this.category,
+    @required this.id,
+    @required this.isFeatured,
+    @required this.name,
+    @required this.price,
+  })  : assert(category != null, 'category must not be null'),
+        assert(id != null, 'id must not be null'),
+        assert(isFeatured != null, 'isFeatured must not be null'),
+        assert(name != null, 'name must not be null'),
+        assert(price != null, 'price must not be null');
+
+  final Category category;
+  final int id;
+  final bool isFeatured;
+  final String name;
+  final int price;
+
+  String get assetName => 'assets/$id-0.jpg';
+
+  @override
+  String toString() => '$name (id=$id)';
+}
--- sample_03_material/lib/routes/home.dart
+++ sample_03_material/lib/routes/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../models/products_repository.dart';
+import '../models/product.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -39,32 +42,51 @@ class HomePage extends StatelessWidget {
           crossAxisCount: 2,
           padding: EdgeInsets.all(16),
           childAspectRatio: 8.0 / 9.0,
-          children: <Widget>[
-            Card(
+          children: _buildGridCards(),
+        ),
+      );
+
+  /// 构建网格卡片集合
+  List<Card> _buildGridCards() {
+    // 1. 加载所有商品
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    // 2. 生成商品列表
+    return products
+        .map((product) => Card(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   AspectRatio(
                     aspectRatio: 18.0 / 11.0,
-                    child: Image.asset('assets/diamond.png'),
+                    child: Image.asset(
+                      product.assetName,
+                      fit: BoxFit.fitWidth,
+                    ),
                   ),
                   Padding(
                     padding: EdgeInsets.fromLTRB(16, 12, 16, 8),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text('标题'),
+                        Text(
+                          product.name,
+                          maxLines: 1,
+                        ),
                         SizedBox(
                           height: 8,
                         ),
-                        Text('详细信息')
+                        Text('￥ ' + product.price.toString())
                       ],
                     ),
-                  ),
+                  )
                 ],
               ),
-            ),
-          ],
-        ),
-      );
+            ))
+        .toList();
+  }
 }
